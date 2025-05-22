ae42a3ad4d68fae22bf02f94d1be4ffe45924607
前端架构师 <62106726+qdleader@users.noreply.github.com>
2025-05-22 23:40:50 +0800

    java 优化ChatController注解及导入

--- src/main/java/com/example/langchain4jmultimodal/controller/ChatController.java
+++ src/main/java/com/example/langchain4jmultimodal/controller/ChatController.java
@@ -3,13 +3,9 @@ package com.example.langchain4jmultimodal.controller;
 
 import com.example.langchain4jmultimodal.request.BaseModelRequestVO;
 import com.example.langchain4jmultimodal.service.ChatService;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import jakarta.annotation.Resource;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 
 @RestController
